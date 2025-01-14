f5c67a120d818058a097192589844391ec64f07d
Kristentf <kristentf2@gmail.com>
2025-01-13 23:25:39 -0300

    Ex02 melhorado
    Resolvi o bug de Idade sem valor e adicionei também a verificação de idade com uma mensagem dizendo se é Bebê, Criança, adolescente, adulto ou Idoso

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ex site01 Curso em video
  Primeiro site com JS, HTML, CSS do curso de Java Screip do canal Curso em Video
+2
--- exJS/ex02/ex02.js
+++ exJS/ex02/ex02.js
@@ -1,8 +1,23 @@
 function verificar() {
-    var nasc = document.querySelector('input#txtnasc')
+    var nasc = document.getElementById('txtnasc')
     var res = document.getElementById('res')
     var data = new Date()
     var ano = data.getFullYear()
-    alert(nasc)
-    res.innerHTML = `Você nasceu em ${nasc} e estamos em ${ano}`
+    var idade = (ano - nasc.value)
+    var sexo = document.getElementsByName('redsex')
+
+    if (nasc.value > ano){
+        alert('[ERRO] tente novamente')
+    } else if (idade < 4){
+        res.innerHTML = `Detectamos um bebê do sexo ${sexo} com ${idade} anos`
+    } else if (idade < 13) {
+        res.innerHTML = `Você é criança e tem ${idade} anos`
+    } else if (idade < 18) {
+        res.innerHTML = `Você é adolecente e tem ${idade} anos`
+    } else if (idade < 60) {
+        res.innerHTML = `Você é adulto e tem ${idade} anos`
+    } else {
+        res.innerHTML = `Você é idoso e tem ${idade} anos`
+    }
+
 }
