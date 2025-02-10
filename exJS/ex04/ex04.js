581e7b0ea8e9d6dc226e2e80e5b8a552fc1c3a87
Kristentf <kristentf2@gmail.com>
2025-02-10 13:30:31 -0300

    alteração no JS
    Fechei a função com {}.
    Ainda não está funcionando corretamente, mas assim que possível irei resolver o BUG

--- exJS/ex04/ex04.js
+++ exJS/ex04/ex04.js
@@ -1,9 +1,9 @@
-function contar() //Função para contar pegando com parametro o inicio, ifm e o passo. 
+function contar(){ //Função para contar pegando com parametro o inicio, ifm e o passo. 
     let inicio = document.getElementById('inicio')
     let fim = document.getElementById('fim')
     let passo = document.getElementById('passo')
     const contagem = document.getElementById('contagem')
 
-    contagem.innerHTML = (inicio, fim, passo)
-
+    contagem.innerHTML = inicio, fim, passo
+}
     
