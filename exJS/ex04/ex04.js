335335af068b6f7edd94dc1464029dd5a52c30ae
Kristentf <kristentf2@gmail.com>
2025-02-10 13:22:36 -0300

    Início ex04
    HTML: Criei todo o esqueleto base do código projeto, como 3 input's, um para início da contagem, um para o final e mais um para o número de passos a ser dado. De resto adicionei um button para ativar a Function e fazer a contagem, (no momento ainda não funciona, pois não finalizei a Function).
    
    CSS: Fiz o css quase inteiro, deixando padronizado e com margem para futuras mudanças.
    
    JS: Apenas iniciei o JS com a criação da Function de calculo, mas ainda não a finalizei.

---
+++ exJS/ex04/ex04.js
@@ -0,0 +1,9 @@
+function contar() //Função para contar pegando com parametro o inicio, ifm e o passo. 
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    const contagem = document.getElementById('contagem')
+
+    contagem.innerHTML = (inicio, fim, passo)
+
+    
