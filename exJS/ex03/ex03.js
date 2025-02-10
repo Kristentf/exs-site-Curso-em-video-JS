f501174183c09df558bf13ad7079b99afd2e50ea
Kristentf <kristentf2@gmail.com>
2025-02-10 11:50:39 -0300

    bug de alert corrigido
    Consegui corrigir o Bug do alert adicionando um trim() para remover os espaços do input. Assim ele identifica que realmente não existe nenhum número e mostra o alert na hora certa, pedindo para digitar algo.

--- exJS/ex03/ex03.js
+++ exJS/ex03/ex03.js
@@ -1,16 +1,20 @@
 const numero = document.getElementById('numero')
 const tab = document.getElementById('tab')
 
-function tabuada(){
-        tab.innerHTML = ''
-        let c = 0
-        let num = numero.value
-        while (c <= 10){
-            let item = document.createElement('option')
-            item.innerText = `${num} x ${c} = ${num * c}`
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
-      
-    }
+function tabuada(){ 
+        if (numero.value.trim() === "") {
+            alert('Digite um número na caixa')
+            return
+        }else{
+            tab.innerHTML = '';
+            let c = 0;
+            let num = numero.value;
+            while (c <= 10){
+                let item = document.createElement('option')
+                item.innerText = `${num} x ${c} = ${num * c}`
+                item.value = `tab${c}`
+                tab.appendChild(item)
+                c++
+            }
+        }
 }
